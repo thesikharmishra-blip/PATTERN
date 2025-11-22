6b5b9be96c0e39e67009bbf4581a0c014a319aca
thesikharmishra-blip <thesikharmishra@gmail.com>
2025-11-22 23:01:14 +0530

    Create InvertedRightAngled.cpp

---
+++ InvertedRightAngled.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void pattern4(int n) {
+        for(int i =1;i<=n;i++){
+            for(int j=1;j<=i-1;j++){
+                cout<<" ";
+            }
+            for(int j=1;j<=2*n-2*i+1 ;j++){
+                cout<<"*";
+            } 
+            cout<<endl;
+
+        }
+
+
+    }
+};
